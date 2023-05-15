30dcc6eda7d64ba05c9ad6c2473f91f7fcc3f370
Nayan <nayan@auberginesolutions.com>
2023-05-15 11:58:41 +0530

    Export files

--- lib/ar_flutter_plugin.dart
+++ lib/ar_flutter_plugin.dart
@@ -1,18 +1,16 @@
-export 'package:ar_flutter_plugin/widgets/ar_view.dart';
+library ar_flutter_plugin;
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class ArFlutterPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('ar_flutter_plugin');
-
-  /// Private constructor to prevent accidental instantiation of the Plugin using the implicit default constructor
-  ArFlutterPlugin._();
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'datatypes/anchor_types.dart';
+export 'datatypes/config_planedetection.dart';
+export 'datatypes/hittest_result_types.dart';
+export 'datatypes/node_types.dart';
+export 'managers/ar_anchor_manager.dart';
+export 'managers/ar_location_manager.dart';
+export 'managers/ar_object_manager.dart';
+export 'managers/ar_session_manager.dart';
+export 'models/ar_anchor.dart';
+export 'models/ar_hittest_result.dart';
+export 'models/ar_node.dart';
+export 'utils/json_converters.dart';
+export 'utils/method_channel.dart';
+export 'widgets/ar_view.dart';
--- test/ar_flutter_plugin_test.dart
+++ test/ar_flutter_plugin_test.dart
@@ -1,6 +1,6 @@
+import 'package:ar_flutter_plugin/utils/method_channel.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('ar_flutter_plugin');
